2b1b12f585042a455d4d5f2d900a5df3206efd43
Quí Nguyễn Phú <qui.nguyen@thegioiwhey.com>
2024-01-09 12:01:50 +0700

    test push in dev

--- src/index.ts
+++ src/index.ts
@@ -116,7 +116,7 @@ const startServer = async () => {
   //   });
   // });
   // app.use(responseHandle);
-
+  //
   const port = process.env.PORT || 3000;
   app.listen(port);
   logger.info("Server is running at http://localhost:" + port);
